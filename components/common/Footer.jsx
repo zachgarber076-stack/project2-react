6945f230dee38ef7e0da219fd10a6b9b1917062b
zachgarber076-stack <zachgarber076@gmail.com>
2025-11-20 17:41:00 -0500

    Initial commit with project files

--- components/common/Footer.jsx
+++ components/common/Footer.jsx
@@ -1,6 +1,6 @@
 function Footer(){
     return(
-        <footer className='bg-[#401E53] py-4 fixed bottom-0 left-0 w-full text-white text-center'>
+        <footer className='bg-[#401E53] py-4 w-full text-white text-center'>
             <p>©2025 ReactLabs</p>
         </footer>
     );
--- components/postList/PostList.jsx
+++ components/postList/PostList.jsx
@@ -1,21 +1,28 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router';
 
 
 function PostList() {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        fetch(`https://jsonplaceholder.typicode.com/posts`)
+            .then(res => res.json())
+            .then(data => setPosts(data.slice(0,20)));
+    }, []);
 
     return (
         <div className='max-w-3xl mx-auto mt-10'>
             <h1 className="text-4xl font-bold mb-6 text-center">Blog Posts</h1>
 
             <div className="grid md:grid-cols-2 gap-6">
-                {[1, 2, 3].map((id) => (
+                {posts.map((post) => (
                     <Link
-                        key={id}
-                        to={`/posts/${id}`}
+                        key={post.id}
+                        to={`/posts/${post.id}`}
                         className="block p-6 bg-white shadow-md rounded-xl border hover:shadow-lg hover:-translate-y-1 transition"
                     >
-                        <h2 className="text-2xl font-semibold mb-2">Post #{id}</h2>
+                        <h2 className="text-2xl font-semibold mb-2">{post.title}</h2>
                         <p className="text-gray-600">
                             Click to read this blog post.
                         </p>
